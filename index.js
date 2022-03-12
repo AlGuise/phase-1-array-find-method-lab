b0fa3309b1de9675d1c40fc1f6f7bd4848496e5e
Alain Giessel <alain.giessel@gmail.com>
2022-03-12 12:23:40 -0700

    commit tree cc074955a099e9d7514b1876d832e54e05eed3b6

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+// const superbowlWin = (array) => {
+//     array.find("W")
+//     if (array.find === true){
+//         return array.year
+//     }
+// }
+
+const superbowlWin = (record) => {
+    const result = record.find(record => record.result === "W");
+    return !!result ? result.year : undefined;
+}
